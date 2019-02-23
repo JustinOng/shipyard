9321d042999d72bea646d01cd952010510db6115
Justin Ong <justin.ong.97@gmail.com>
2019-02-23 14:58:56 +0800

    Create build environment for React and Ant Design

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,10 @@
-import { Terminal } from "xterm";
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App.jsx";
+
+ReactDOM.render(<App/>, document.querySelector("#app"));
+/*import { Terminal } from "xterm";
 import * as attach from "xterm/lib/addons/attach/attach";
 
 Terminal.applyAddon(attach);
@@ -11,10 +17,11 @@ function initTerminal() {
   console.log("Initialising terminal");
   const term = new Terminal();
   term.open(document.querySelector("#terminal"));
+  term.setOption("cursorStyle", "block");
 
   const socket = new WebSocket(`ws://${location.host}/ws`);
 
   socket.onopen = () => {
     term.attach(socket);
   }
-}
+}*/
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./public/js/main.js",
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -14,5 +15,31 @@ module.exports = {
       }
     ])
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader",
+          {
+            loader: "less-loader",
+            options: {
+              javascriptEnabled: true
+            }
+          }
+        ]
+      }
+    ]
+  },
   devtool: "source-map"
 };
